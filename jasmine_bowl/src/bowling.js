3bad8afcf5007b9bca49d4a35ce98bbc272defee
Alex Malkevich <malkevich.alex@gmail.com>
2014-11-21 00:01:14 +0200

    Make jasmine bowling tests

--- jasmine_bowl/src/bowling.js
+++ jasmine_bowl/src/bowling.js
@@ -11,40 +11,28 @@ var bwScore = {
             if (f.skittlesPerFrame.attempt1 == 10) f.skittlesPerFrame.attempt2 = 0;
             f.bonus = 0;
             this.frameSet.push(f);
-        } else {
-            this.frameSet[frameCount].skittlesPerFrame.attempt2 = skit;
-        }
+        } else this.frameSet[frameCount].skittlesPerFrame.attempt2 = skit;
         return JSON.stringify(this.frameSet);
     },
     addBonus: function (frameCount, addBonusSteps) {
-        if (this.frameSet[frameCount + 1] === undefined) {
-            return 'No next step available';
-        } else {
-            // bonus for strikes
+        if (this.frameSet[frameCount + 1] === undefined) return 'No next step available';
+        else {
             if (addBonusSteps == 2) {
                 if (this.frameSet[frameCount + 1].skittlesPerFrame.attempt1 != 10) {
-                    //to implement validation if attempt1 or attempt2 is undefined
                     this.frameSet[frameCount].bonus = this.frameSet[frameCount + 1].skittlesPerFrame.attempt1 + this.frameSet[frameCount + 1].skittlesPerFrame.attempt2;
                 } else {
                     this.frameSet[frameCount].bonus = this.frameSet[frameCount + 1].skittlesPerFrame.attempt1 + this.frameSet[frameCount + 2].skittlesPerFrame.attempt1;
                 }
             }
-            // bonus for spare
-            if (addBonusSteps == 1) {
-                this.frameSet[frameCount].bonus += this.frameSet[frameCount + 1].skittlesPerFrame.attempt1
-            }
+            if (addBonusSteps == 1) this.frameSet[frameCount].bonus += this.frameSet[frameCount + 1].skittlesPerFrame.attempt1;
             return JSON.stringify(this.frameSet);
         }
     },
 
     count: function () {
-        var frameSetLenght, i, sum;
-        frameSetLenght = this.frameSet.length;
-        i = 0;
-        sum = 0
-        for (i; i < frameSetLenght; i++) {
-            sum += this.frameSet[i].skittlesPerFrame.attempt1 + this.frameSet[i].skittlesPerFrame.attempt2 + this.frameSet[i].bonus;
-        }
-        return sum;
+        var frameSetLength = this.frameSet.length, i = 0, summat = 0;
+        for (i; i < frameSetLength; i++)
+            summat += this.frameSet[i].skittlesPerFrame.attempt1 + this.frameSet[i].skittlesPerFrame.attempt2 + this.frameSet[i].bonus;
+        return summat;
     }
 };
